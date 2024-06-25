f6ed74d2424d439c84f91b84fb736948dbbbe223
Leonard Paturel <leonardpaturel@gmail.com>
2024-06-25 12:17:15 +0100

    fix gitsigns

--- lua/leo/plugins/gitsigns.lua
+++ lua/leo/plugins/gitsigns.lua
@@ -2,28 +2,24 @@ return {
 	"lewis6991/gitsigns.nvim",
 	event = { "BufReadPre", "BufNewFile" },
 	opts = {
-		on_attach = function(bufnr)
+		on_attach = function()
 			local gitsign = package.loaded.gitsigns
 
-			local function map(mode, l, r, desc)
-				vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
-			end
+			vim.keymap.set("n", "<leader>hp", gitsign.preview_hunk_inline, { desc = "Preview hunk" })
+			vim.keymap.set("n", "<leader>td", gitsign.toggle_deleted, { desc = "Toggle deleted" })
+			vim.keymap.set("n", "]h", gitsign.next_hunk, { desc = "Next Hunk" })
+			vim.keymap.set("n", "[h", gitsign.prev_hunk, { desc = "Prev Hunk" })
 
-			map("n", "<leader>hp", gitsign.preview_hunk_inline, "Preview hunk")
-			map("n", "<leader>td", gitsign.toggle_deleted)
-			map("n", "]h", gitsign.next_hunk, "Next Hunk")
-			map("n", "[h", gitsign.prev_hunk, "Prev Hunk")
-
-			map("v", "<leader>hr", function()
+			vim.keymap.set("v", "<leader>hr", function()
 				gitsign.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-			end, "Reset hunk")
+			end, { desc = "Reset hunk" })
 
-			map("n", "<leader>hR", gitsign.reset_buffer, { desc = "Reset buffer" })
+			vim.keymap.set("n", "<leader>hR", gitsign.reset_buffer, { desc = "Reset buffer" })
 
-			map("n", "<leader>hd", gitsign.diffthis, "Diff this")
+			vim.keymap.set("n", "<leader>hd", gitsign.diffthis, { desc = "Diff this" })
 
 			-- Text object
-			map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "Gitsigns select hunk")
+			vim.keymap.set({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "Gitsigns select hunk" })
 		end,
 	},
 }
