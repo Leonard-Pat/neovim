f1cefeb2bcc28eb921c0ec8bfea3c516058107a6
Leonard Paturel <leonardpaturel@gmail.com>
2024-07-03 15:51:00 +0100

    redid config

---
+++ lua/leo/plugins/lsp/illuminate.lua
@@ -0,0 +1,33 @@
+return {
+	"RRethy/vim-illuminate",
+	config = function()
+		-- default configuration
+		require("illuminate").configure({
+			-- providers: provider used to get references in the buffer, ordered by priority
+			providers = {
+				"lsp",
+				"treesitter",
+				"regex",
+			},
+			-- delay: delay in milliseconds
+			delay = 100,
+
+			-- filetypes_denylist: filetypes to not illuminate, this overrides filetypes_allowlist
+			filetypes_denylist = {
+				"dirbuf",
+				"dirvish",
+				"fugitive",
+			},
+
+			under_cursor = true,
+
+			min_count_to_highlight = 1,
+
+			should_enable = function(bufnr)
+				return true
+			end,
+			-- case_insensitive_regex: sets regex case sensitivity
+			case_insensitive_regex = false,
+		})
+	end,
+}
